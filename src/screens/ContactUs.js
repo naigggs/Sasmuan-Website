28cc1f566c56da76d857d357893d130a2379e98a
naigggs <giancabigting1026@gmail.com>
2023-10-31 17:03:54 +0800

    updated contactpage

--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../components/Footer";
 import HeroContactUs from "../components/Hero_ContactUs";
 import Default from "../medias/Profile Pictures/Default.jpg";
 import Gian from "../medias/Profile Pictures/GianCabigting.jpg";
@@ -13,7 +14,10 @@ function ContactUs() {
       <HeroContactUs />
       <div class="flex justify-center bg-gradient-to-b from-blue-200 via-blue-400 to-blue-900">
         <div class="bg-white w-1180 h-min rounded-3xl p-20 shadow-md my-10">
-          <div id="meet-our-team" class="Inter text-center text-gray-800 uppercase text-shadow-md font-extrabold text-4xl sm:text-7xl mb-16">
+          <div
+            id="meet-our-team"
+            class="Inter text-center text-gray-800 uppercase text-shadow-md font-extrabold text-4xl sm:text-7xl mb-16"
+          >
             Meet Our Team
           </div>
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 mb-12">
@@ -28,7 +32,9 @@ function ContactUs() {
               </div>
               <p class="mt-[1px] text-lg text-center italic">Project Manager</p>
               <p class="mt-[3px] text-sm text-center">0991-348-7830</p>
-              <p class="mt-[2px] text-base text-center">giancabigting1026@gmail.com</p>
+              <p class="mt-[2px] text-base text-center">
+                giancabigting1026@gmail.com
+              </p>
             </div>
             <div class="m-[8px]">
               <img
@@ -65,7 +71,9 @@ function ContactUs() {
               <div class="text-center text-2xl font-bold mt-[20px]">
                 Albert Gonzales
               </div>
-              <p class="mt-[1px] text-lg text-center italic">Graphic Designer</p>
+              <p class="mt-[1px] text-lg text-center italic">
+                Graphic Designer
+              </p>
               <p class="mt-[3px] text-sm text-center">123-456-7890</p>
               <p class="mt-[2px] text-base text-center">gian@example.com</p>
             </div>
@@ -86,6 +94,58 @@ function ContactUs() {
         </div>
       </div>
 
+      <div class="flex flex-wrap justify-center gap-4 sm:gap-8 md:gap-12 lg:gap-20 p-4 sm:p-8 md:p-12 lg:p-20">
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/5">
+          <h2 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-gray-800 uppercase font-extrabold mb-6 sm:mb-10">
+            Our Mission
+          </h2>
+          <p class="mission">
+            Our team is dedicated to showcasing the hidden gems, serene beauty,
+            and cultural heritage of Sasmuan, Pampanga. Through our tourism
+            website, we aim to inspire travelers to explore this picturesque
+            town, fostering sustainable tourism that benefits both visitors and
+            the local community. By providing comprehensive information,
+            captivating visuals, and engaging stories, we aspire to make Sasmuan
+            a must-visit destination, where every traveler can forge
+            unforgettable memories while contributing to the preservation of its
+            natural wonders and vibrant traditions.
+          </p>
+        </div>
+
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/5">
+          <h2 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-gray-800 uppercase font-extrabold mb-6 sm:mb-10">
+            Our Vision
+          </h2>
+          <p class="vision">
+            We aspire to be the foremost digital storytellers, weaving
+            captivating narratives that illuminate the essence of Sasmuan,
+            Pampanga. Our tourism website will serve as an immersive canvas,
+            painting a vivid picture of a town where tranquility and tradition
+            coalesce, leaving an indelible mark on every traveler's heart. With
+            unwavering commitment, our team's vision is to mold the digital
+            portal into a conduit of exploration, enlightenment, and
+            inspiration, casting Sasmuan, Pampanga, in a spotlight that
+            illuminates its unique blend of tranquility and tradition to the
+            world.
+          </p>
+        </div>
+
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/5">
+          <h2 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-gray-800 uppercase font-extrabold mb-6 sm:mb-10">
+            Our Objectives
+          </h2>
+          <p class="vision">
+            To revamp and enrich the official tourism website of Sasmuan,
+            Pampanga, creating a comprehensive and engaging online platform that
+            promotes the town's unique cultural heritage, natural attractions,
+            and local experiences, thereby increasing visitor engagement,
+            boosting tourism, and fostering a sense of pride among both
+            residents and tourists.
+          </p>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -10,74 +10,72 @@ function LandingPage() {
     <div>
       <Hero />
       <div class="Inter p-5 flex flex-col justify-center items-center">
-  <div class="Inter text-center text-gray-800 uppercase text-shadow-md  font-extrabold text-4xl sm:text-7xl">
-    Sasmuan's Rich Tapestry
-  </div>
-  <div class="bg-white rounded-lg mx-auto p-4 max-w-screen-md sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
-    <p class="text-gray-700 text-lg my-5">
-      Through the captivating town of Sasmuan, nestled in the heart of
-      Pampanga, Philippines. Immerse yourself in the rich culture,
-      stunning landscapes, and vibrant local life that make Sasmuan a
-      truly unique destination.
-    </p>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      <div class="bg-white rounded-lg p-3">
-        <h3 class="text-lg font-semibold text-gray-700">
-          Cultural Delights
-        </h3>
-        <p class="text-gray-600">
-          Delve into Sasmuan's history and traditions as you wander
-          through its charming streets. Discover centuries-old churches,
-          vibrant festivals, and a warm-hearted community eager to share
-          their heritage.
-        </p>
+        <div class="Inter text-center text-gray-800 uppercase text-shadow-md  font-extrabold text-4xl sm:text-7xl">
+          Sasmuan's Rich Tapestry
+        </div>
+        <div class="bg-white rounded-lg mx-auto p-4 max-w-screen-md sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
+          <p class="text-gray-700 text-lg my-5">
+            Through the captivating town of Sasmuan, nestled in the heart of
+            Pampanga, Philippines. Immerse yourself in the rich culture,
+            stunning landscapes, and vibrant local life that make Sasmuan a
+            truly unique destination.
+          </p>
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div class="bg-white rounded-lg p-3">
+              <h3 class="text-lg font-semibold text-gray-700">
+                Cultural Delights
+              </h3>
+              <p class="text-gray-600">
+                Delve into Sasmuan's history and traditions as you wander
+                through its charming streets. Discover centuries-old churches,
+                vibrant festivals, and a warm-hearted community eager to share
+                their heritage.
+              </p>
+            </div>
+
+            <div class="bg-white rounded-lg p-3">
+              <h3 class="text-lg font-semibold text-gray-700">
+                Natural Wonders
+              </h3>
+              <p class="text-gray-600">
+                Be awe-inspired by the breathtaking beauty of Sasmuan's natural
+                landscapes. From serene rice fields to lush mangrove forests,
+                there's no shortage of stunning scenery to explore.
+              </p>
+            </div>
+
+            <div class="bg-white rounded-lg p-3">
+              <h3 class="text-lg font-semibold text-gray-700">
+                Gastronomic Adventures
+              </h3>
+              <p class="text-gray-600">
+                Indulge in the flavors of Pampanga with Sasmuan's delectable
+                cuisine. From savory sisig to mouthwatering halo-halo, your
+                taste buds are in for a treat.
+              </p>
+            </div>
+
+            <div class="bg-white rounded-lg p-3">
+              <h3 class="text-lg font-semibold text-gray-700">
+                Local Artistry
+              </h3>
+              <p class="text-gray-600">
+                Immerse yourself in the local arts and crafts scene, where you
+                can witness skilled artisans creating intricate works that
+                reflect the soul of Sasmuan.
+              </p>
+            </div>
+          </div>
+
+          <p class="text-gray-700 text-lg my-5">
+            Whether you're an avid traveler or a curious explorer, our "About
+            This Trip" section is your gateway to uncovering the hidden gems and
+            unforgettable experiences that await you in Sasmuan, Pampanga,
+            Philippines. Join us as we embark on this virtual adventure
+            together!
+          </p>
+        </div>
       </div>
-
-      <div class="bg-white rounded-lg p-3">
-        <h3 class="text-lg font-semibold text-gray-700">
-          Natural Wonders
-        </h3>
-        <p class="text-gray-600">
-          Be awe-inspired by the breathtaking beauty of Sasmuan's natural
-          landscapes. From serene rice fields to lush mangrove forests,
-          there's no shortage of stunning scenery to explore.
-        </p>
-      </div>
-
-      <div class="bg-white rounded-lg p-3">
-        <h3 class="text-lg font-semibold text-gray-700">
-          Gastronomic Adventures
-        </h3>
-        <p class="text-gray-600">
-          Indulge in the flavors of Pampanga with Sasmuan's delectable
-          cuisine. From savory sisig to mouthwatering halo-halo, your
-          taste buds are in for a treat.
-        </p>
-      </div>
-
-      <div class="bg-white rounded-lg p-3">
-        <h3 class="text-lg font-semibold text-gray-700">
-          Local Artistry
-        </h3>
-        <p class="text-gray-600">
-          Immerse yourself in the local arts and crafts scene, where you
-          can witness skilled artisans creating intricate works that
-          reflect the soul of Sasmuan.
-        </p>
-      </div>
-    </div>
-
-    <p class="text-gray-700 text-lg my-5">
-      Whether you're an avid traveler or a curious explorer, our "About
-      This Trip" section is your gateway to uncovering the hidden gems and
-      unforgettable experiences that await you in Sasmuan, Pampanga,
-      Philippines. Join us as we embark on this virtual adventure
-      together!
-    </p>
-  </div>
-</div>
-
-
       <Footer />
     </div>
   );
